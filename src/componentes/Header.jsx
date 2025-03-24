67a418140c5e6a7847fc4fe0758af10774a72a34
laryTgs <laryssatortato99@gmail.com>
2025-03-24 14:00:02 -0300

    header feito

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,12 +1,23 @@
 function Header() {
-  <div className="bg-gray-600 flex">
-    <h1 className="text-sky-600">Dev.Tgs</h1>
-    <div>
-      <img src="/img/logo/github.png" />
-      <img src="/img/logo/linkedin.png" />
-      <img src="/img/logo/instagram.png" />
+  return (
+    <div className="bg-gray-950 flex p-5 justify-between">
+      <h1 className="text-sky-600 font-bold text-xl font-mono">Dev.Tgs</h1>
+      <div className="flex gap-4 cursor-pointer">
+        <a href="https://github.com/Larytgs" target="blank">
+          <img src="/img/logo/github.png" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/laryssa-tortato-7612961a9/"
+          target="blank"
+        >
+          <img src="/img/logo/linkedin.png" />
+        </a>
+        <a href="https://www.instagram.com/larytgs/" target="blank">
+          <img className="mr-4" src="/img/logo/instagram.png" />
+        </a>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default Header;
