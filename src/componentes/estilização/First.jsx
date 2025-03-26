74410db6203a49f3c9c30c6ed5d5bd7fcd5300d4
laryTgs <laryssatortato99@gmail.com>
2025-03-26 17:29:14 -0300

    Update First.jsx

--- src/componentes/estilização/First.jsx
+++ src/componentes/estilização/First.jsx
@@ -1,6 +1,9 @@
-function First(props) {
+function First(onClick, props) {
   return (
-    <button className="text-3xl pt-[10px] pb-[10px] pl-[70px] pr-[70px]">
+    <button
+      onClick={props.onClick}
+      className="text-3xl pt-[10px] pb-[10px] pl-[70px] pr-[70px]"
+    >
       {props.children}
     </button>
   );
