7c82f2f986cf58aeddc303d22836b4a1a9982da6
laryTgs <laryssatortato99@gmail.com>
2025-03-27 10:16:03 -0300

    add um cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import CustomCursor from "./componentes/estilização/CustomCursor";
 import AppRoutes from "./routes";
 
 function App() {
   return (
     <>
       <div>
+        <CustomCursor /> {/* O cursor */}
         <AppRoutes />
       </div>
     </>
