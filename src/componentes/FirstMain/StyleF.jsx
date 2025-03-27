6adc90bca7bbb7cde980d56251d8a14e3a946569
laryTgs <laryssatortato99@gmail.com>
2025-03-27 18:19:34 -0300

    Update StyleF.jsx

--- src/componentes/FirstMain/StyleF.jsx
+++ src/componentes/FirstMain/StyleF.jsx
@@ -2,9 +2,24 @@
 
 function Ul(props) {
   return (
-    <ul className="text-center max-md:flex max-md:justify-between">
+    <ul
+      className="text-center
+      max-lg:flex max-lg:justify-center"
+    >
       {props.children}
     </ul>
+  );
+}
+
+function Lista(props) {
+  return (
+    <li
+      className="inline-block m-1 bg-white text-black 
+      max-lg:flex-1 max-lg:flex  
+      max-md:m-0.5"
+    >
+      {props.children}{" "}
+    </li>
   );
 }
 
@@ -13,22 +28,12 @@ function Button(props) {
     <button
       onClick={props.onClick}
       className="text-3xl py-[10px] px-[70px] relative 
-      max-md:text-[1.4em] max-md:w-[100%]
+      max-lg:text-[1.5em] max-lg:w-[100%] max-lg:py-[10px] max-lg:px-[35px]
+      max-md:text-[1.4em] 
       max-md:py-2 max-md:px-3"
     >
       {props.children}
     </button>
-  );
-}
-
-function Lista(props) {
-  return (
-    <li
-      className="inline-block m-1 bg-white text-black 
-    max-md:flex-1 max-md:flex max-md:m-0.5"
-    >
-      {props.children}{" "}
-    </li>
   );
 }
 
